e5e9e1500faf40679112973549f322dc6b238719
begumgokmen <bg2838@barnard.edu>
2024-03-29 15:36:00 -0400

    initial commit

---
+++ build/precache-manifest.182ec75cb5750d1074ae529b76e6edb4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b3d411fdecc412425f58fa49c1b11c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e656b340660b1eef9a8",
+    "url": "/static/js/2.64de71d0.chunk.js"
+  },
+  {
+    "revision": "11c1dbb92bc95d45f401",
+    "url": "/static/js/main.3b8bb234.chunk.js"
+  },
+  {
+    "revision": "2f55955791534fe0d08b",
+    "url": "/static/js/runtime-main.1048f588.js"
+  }
+]);
